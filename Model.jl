a214599d0b22a6a37976481cad504464d55f17ad
pockos56 <103532988+pockos56@users.noreply.github.com>
2022-04-21 11:50:22 +0200

    Model v0.1
    What I have from notes with Saer

--- Model.jl
+++ Model.jl
@@ -1,2 +1,21 @@
-using BSON
-BSON.@load("C:\\Users\\alex_\\Documents\\GitHub\\RTI_prediction\\Nice descriptors", nice_desc)
+using DataFrames
+using Statistics
+using ScikitLearn
+using CSV
+using Plots
+# Notes
+# Function with leverage
+#  80-20 training test 5-fold 
+#X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=400, test_size=200, random_state=4)
+# CrossValidation
+########################
+# Importing the data
+gr = CSV.read("C:\\Users\\alex_\\Documents\\GitHub\\RTI_prediction\\Refined_Norm_GR.csv",DataFrame)
+am = CSV.read("C:\\Users\\alex_\\Documents\\GitHub\\RTI_prediction\\Refined_Norm_Amide.csv", DataFrame)
+
+
+############
+# Regression
+
+RTI_col =
+First_desc_col =
