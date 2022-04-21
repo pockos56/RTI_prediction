c6309bbcf3eb0fdc5733eda667d8a1f26efc8e75
pockos56 <103532988+pockos56@users.noreply.github.com>
2022-04-21 18:53:03 +0200

    Update Model.jl

--- Model.jl
+++ Model.jl
@@ -8,11 +8,10 @@ using PyCall
 
 import StatsPlots as sp
 
-
 using ScikitLearn.CrossValidation: cross_val_score
 using ScikitLearn.CrossValidation: train_test_split
 @sk_import ensemble: RandomForestRegressor
-########################
+#################################
 # Notes
 # Function with leverage
 #  80-20 training test split , 5-fold CrossValidation
@@ -21,18 +20,34 @@ using ScikitLearn.CrossValidation: train_test_split
 # X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=400, test_size=200, random_state=4)
 #
 #
-########################
-# Importing the data
+#################################
+## Importing the data
+
+# Change data from here (gre or am)
 am = CSV.read("C:\\Users\\alex_\\Documents\\GitHub\\RTI_prediction\\Refined_Norm_Amide.csv", DataFrame)
 # GR = CSV.read("C:\\Users\\alex_\\Documents\\GitHub\\RTI_prediction\\Refined_Norm_GR.csv", DataFrame)
-# Change data from here (gre or am)
 data = am
-data_name = "am"
+data_name = "Amide"
 
-Y = data[:,2]
-X = Matrix(data[:,8:end])
+RTI = data[:,2]
+desc = Matrix(data[:,8:end])
 
-########################
-# Regression
+#################################
+sp.scatter(RTI, desc, normalized=true, label=false)
+xlims!(-1000,1000)
+ylims!(-5000,5000)
 
-sp.histogram(Y,X)
+
+#################################
+## Regression
+
+MaxFeat = Int(round(sqrt(size(desc,2))))
+reg = RandomForestRegressor(n_estimators=100, min_samples_leaf=7,verbose =1, oob_score =true, max_features= MaxFeat)
+## QUESTION: WHAT IS VERBOSITY?
+
+
+X_train, X_test, y_train, y_test = train_test_split(desc, RTI, test_size=0.20, random_state=21);
+fit!(reg, X_train, y_train)
+accuracy = score(reg, X_train, y_train)
+n_folds=5
+cross_val_score(reg, X_train, y_train; cv=n_folds)
