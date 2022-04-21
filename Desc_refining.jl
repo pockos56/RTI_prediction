ec83bd3d9904fc404ec962577d3c8377705514ca
pockos56 <103532988+pockos56@users.noreply.github.com>
2022-04-21 15:39:14 +0200

    Missing values removal
    Either by compound or by descriptor removal, the am and GR dataset have no missing values

--- Desc_refining.jl
+++ Desc_refining.jl
@@ -1,4 +1,4 @@
-using CSV, DataFrames, Statistics
+using CSV, DataFrames, Statistics, Plots
 #####################
 # Let's check whether our data is the same
 a = CSV.read("C:\\Users\\alex_\\Documents\\GitHub\\RTI_prediction\\Amide_descriptors1.csv", DataFrame)
@@ -99,7 +99,7 @@ for j = 8:size(refined_data,2)
 end
 missing_percentage = (countmissing*100)/((size(refined_data,2)-8)*size(refined_data,1))
 
- # We have found the maxima
+# We have found the maxima
 
 refined_norm_data = copy(refined_data)
 factor = 1000
@@ -117,20 +117,98 @@ for j = 8:size(refined_data,2)
     end
 end
 
-
 refined_norm_data
+
+## Repair the missing and Inf values
+## Missing values
+
+#any(ismissing(refined_norm_data[:,8:end]))
+#refined_norm_data1 = replace(Matrix(refined_norm_data), missing =>NaN)
+#sum(any(ismissing((Matrix(refined_norm_data[:,8:end]))), dims =1))
+#(ismissing.(eachrow(refined_norm_data[:,8:end])))
+
+countmissing = falses(size(refined_norm_data,1), size(refined_norm_data,2))
+for j = 8:size(refined_norm_data,2)
+    for i = 1:size(refined_norm_data,1)
+        if ismissing(refined_norm_data[i,j])
+            countmissing[i,j] = true
+        end
+    end
+end
+heatmap(countmissing, legend = false, label = "Missing values location")
+
+countmissing = falses(size(refined_norm_data,2))
+for i = 1:size(refined_norm_data,1)
+    if ismissing(refined_norm_data[i,1550])
+        countmissing[i] = true
+    end
+end
+
+bad_comps = findall(countmissing)
+heatmap(countmissing, legend = false, label = "Missing values location")
+
+No_missings = refined_norm_data[Not(bad_comps), :]
+
+countmissing = falses(size(No_missings,1), size(No_missings,2))
+for j = 8:size(No_missings,2)
+    for i = 1:size(No_missings,1)
+        if ismissing(No_missings[i,j])
+            countmissing[i,j] = true
+        end
+    end
+end
+heatmap(countmissing, legend = false, label = "Missing values location for No_missings")
+
+bad3 = " "
+for j = 1:size(countmissing,2)
+    if sum(countmissing[:,j]) != 0
+        bad3_temp = names(No_missings, j)
+        bad3 = vcat(bad3,bad3_temp)
+    end
+end
+bad3 = bad3[2:length(bad3)]
+#println("Nothing changes at: $(bad2)")
+ref_norm_nomissing_data = select(No_missings, Not(bad3))            # Removing descriptors with missing values
+
+countmissing = falses(size(ref_norm_nomissing_data,1), size(ref_norm_nomissing_data,2))
+for j = 8:size(ref_norm_nomissing_data,2)
+    for i = 1:size(ref_norm_nomissing_data,1)
+        if ismissing(ref_norm_nomissing_data[i,j])
+            countmissing[i,j] = true
+        end
+    end
+end
+sum(countmissing) # Equals zero :-)
+
+
+## Inf values
+#refined_norm_data1 = replace(Matrix(refined_norm_data), missing =>NaN)
+sum(any(Matrix(ref_norm_nomissing_data[:,8:end]) .== Inf, dims =2))
+# Out of 1488 compounds, 44 have Inf values
+# out of 2197 descriptors, 74 have Inf values
+data_wo_inf = refined_norm_data[:,1:7]
+for i = 8:size(refined_norm_data,2)
+    data_temp = refined_norm_data[:,i]
+    if any((data_temp .== Inf)) == false
+        data_wo_inf = hcat(data_wo_inf,data_temp)
+    end
+end
+
+
+
 ## Exctracting the useful descriptors
 using BSON
-nice_desc = names(refined_data)[8:end]
+nice_desc = names(ref_norm_nomissing_data)[8:end]
 BSON.@save("C:\\Users\\alex_\\Documents\\GitHub\\RTI_prediction\\Nice descriptors", nice_desc)
 
-## Saving the refined normalised AMIDE dataset
-CSV.write("C:\\Users\\alex_\\Documents\\GitHub\\RTI_prediction\\Refined_Norm_Amide.csv", refined_norm_data)
+## Saving the refined normalised AMIDE dataset (without missing values)
+CSV.write("C:\\Users\\alex_\\Documents\\GitHub\\RTI_prediction\\Refined_Norm_Amide.csv", ref_norm_nomissing_data)
 
 ## Refining the Greek dataset
+##!! WE SHOULD HAVE THE SAME DESCRIPTORS WITH THE ref_norm_nomissing_data OF AMIDE DATASET
 gr_raw = CSV.read("C:\\Users\\alex_\\Documents\\GitHub\\RTI_prediction\\GreekDataset_+ESI_Descriptors.csv", DataFrame)
-gr_not_bad1 = select(gr_raw, Not(bad1))            # Removing the high variation descriptors
-
+gr_not_bad = select(gr_raw, Not(bad1))            # Removing the high variation descriptors
+gr_not_bad1 = select(gr_not_bad, Not(bad3))       # Removing the descriptors that showed missing values in the Amide DATASET
 maxima = zeros(size(gr_not_bad1,2))
 for j = 5:size(gr_not_bad1,2)
     maxima[j] = (maximum(collect(skipmissing(gr_not_bad1[!,j]))))
@@ -152,5 +230,32 @@ for j = 5:size(gr_not_bad1,2)
     end
 end
 
+countmissing = falses(size(gr_refined,1), size(gr_refined,2))
+for j = 8:size(gr_refined,2)
+    for i = 1:size(gr_refined,1)
+        if ismissing(gr_refined[i,j])
+            countmissing[i,j] = true
+        end
+    end
+end
+findall(countmissing) # 688, 1043 and 1807 compounds are showing missing values
+heatmap(countmissing)
+    xlims!(0,2200)
+    ylims!(0,2200)
+
+ref_norm_nomissing_data_gr = gr_refined[Not(688,1043,1807), :]
+
+countmissing = falses(size(ref_norm_nomissing_data_gr,1), size(ref_norm_nomissing_data_gr,2))
+for j = 8:size(ref_norm_nomissing_data_gr,2)
+    for i = 1:size(ref_norm_nomissing_data_gr,1)
+        if ismissing(ref_norm_nomissing_data_gr[i,j])
+            countmissing[i,j] = true
+        end
+    end
+end
+
+sum(countmissing) # Equals zero! :)
+
+
 ## Saving the refined normalised AMIDE dataset
-CSV.write("C:\\Users\\alex_\\Documents\\GitHub\\RTI_prediction\\Refined_Norm_GR.csv", gr_refined)
+CSV.write("C:\\Users\\alex_\\Documents\\GitHub\\RTI_prediction\\Refined_Norm_GR.csv", ref_norm_nomissing_data_gr)
--- Model.jl
+++ Model.jl
@@ -3,19 +3,36 @@ using Statistics
 using ScikitLearn
 using CSV
 using Plots
+using LinearAlgebra
+using PyCall
+
+import StatsPlots as sp
+
+
+using ScikitLearn.CrossValidation: cross_val_score
+using ScikitLearn.CrossValidation: train_test_split
+@sk_import ensemble: RandomForestRegressor
+########################
 # Notes
 # Function with leverage
-#  80-20 training test 5-fold 
-#X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=400, test_size=200, random_state=4)
-# CrossValidation
+#  80-20 training test split , 5-fold CrossValidation
+# Minimum number of samples per leaf and number of trees => Optimization
+# Max_features_parameter = Sqrt(#descriptors)
+# X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=400, test_size=200, random_state=4)
+#
+#
 ########################
 # Importing the data
-gr = CSV.read("C:\\Users\\alex_\\Documents\\GitHub\\RTI_prediction\\Refined_Norm_GR.csv",DataFrame)
 am = CSV.read("C:\\Users\\alex_\\Documents\\GitHub\\RTI_prediction\\Refined_Norm_Amide.csv", DataFrame)
+# GR = CSV.read("C:\\Users\\alex_\\Documents\\GitHub\\RTI_prediction\\Refined_Norm_GR.csv", DataFrame)
+# Change data from here (gre or am)
+data = am
+data_name = "am"
 
+Y = data[:,2]
+X = Matrix(data[:,8:end])
 
-############
+########################
 # Regression
 
-RTI_col =
-First_desc_col =
+sp.histogram(Y,X)
