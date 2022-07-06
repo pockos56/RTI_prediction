2a6d13af78b91f92739ee18ceb5d2c14b955b964
pockos56 <103532988+pockos56@users.noreply.github.com>
2022-07-06 21:05:45 +0200

    Small fix to plots

--- make_plots.jl
+++ make_plots.jl
@@ -84,7 +84,7 @@ BSON.@load("C:\\Users\\alex_\\Documents\\GitHub\\RTI_prediction\\Index_GroupF", 
 Norm_raw_=deepcopy(Norm_raw)
 
 inch = Norm_raw_[:,3]
-inch[ismissing.(inch) .== 1] .= "WeMissYou"
+inch[ismissing.(inch) .== 1] .= "WeWillMissYou"
 Norm = convert.(String, inch)
 
 com = Int.(zeros(20))
@@ -118,7 +118,7 @@ for i = 1:length(com)
     z[i,5] = length(findall(x -> x .== com[i],groupE))
     z[i,6] = length(findall(x -> x .== com[i],groupF))
 end
-z_sum = Int.(sum(z,dims=1))
+z_sum = (Int.(sum(z,dims=1))[1,:])
 
 A_ = sort(Norm_raw[groupA,:],[order(:MW)])[:,[:SMILES, :MW]]
 a1 = A_[Int(round(size(A_,1)*0.05)),:]      # No
@@ -139,77 +139,81 @@ c3 = C_[Int(round(size(C_,1)*0.501)),:]       # Yes pubchem
 c4 = C_[Int(round(size(C_,1)*0.553)),:]       # Yes pubchem
 c5 = C_[Int(round(size(C_,1)*0.953)),:]       # No
 D_ = sort(Norm_raw[groupD,:],[order(:MW)])[:,[:SMILES, :MW]]
-d1 = D_[Int(round(size(D_,1)*0.05)),:]       #
-d2 = D_[Int(round(size(D_,1)*0.45)),:]
-d3 = D_[Int(round(size(D_,1)*0.50)),:]
-d4 = D_[Int(round(size(D_,1)*0.55)),:]
-d5 = D_[Int(round(size(D_,1)*0.95)),:]
-d1[1]
+d1 = D_[Int(round(size(D_,1)*0.052)),:]       # Yes, 10.1093/protein/8.3.275
+d2 = D_[Int(round(size(D_,1)*0.456)),:]       # No
+d3 = D_[Int(round(size(D_,1)*0.506)),:]       # No
+d4 = D_[Int(round(size(D_,1)*0.55)),:]        # Yes, 10.1007/s10337-013-2393-y
+d5 = D_[Int(round(size(D_,1)*0.954)),:]       # No
 E_ = sort(Norm_raw[groupE,:],[order(:MW)])[:,[:SMILES, :MW]]
-e1 = E_[Int(round(size(E_,1)*0.05)),:]       #
-e2 = E_[Int(round(size(E_,1)*0.45)),:]
-e3 = E_[Int(round(size(E_,1)*0.50)),:]
-e4 = E_[Int(round(size(E_,1)*0.55)),:]
-e5 = E_[Int(round(size(E_,1)*0.95)),:]
+e1 = E_[Int(round(size(E_,1)*0.06)),:]       # Yes, 10.1016/S0003-2670(01)83774-0
+e2 = E_[Int(round(size(E_,1)*0.45)),:]       # Yes, 10.1081/AL-200043463
+e3 = E_[Int(round(size(E_,1)*0.54)),:]       # No
+e4 = E_[Int(round(size(E_,1)*0.55)),:]       # Yes, Extraction and quantification of phthalates in plastic coca-cola soft drinks using high performance liquid chromatography
+e5 = E_[Int(round(size(E_,1)*0.97)),:]      # No
 
 F_ = sort(Norm_raw[groupF,:],[order(:MW)])[:,[:SMILES, :MW]]
-f1 = F_[Int(round(size(F_,1)*0.06)),:]       #
-f2 = F_[Int(round(size(F_,1)*0.45)),:]
-f3 = F_[Int(round(size(F_,1)*0.50)),:]
-f4 = F_[Int(round(size(F_,1)*0.55)),:]
-f5 = F_[Int(round(size(F_,1)*0.95)),:]
+f1 = F_[Int(round(size(F_,1)*0.065)),:]       # No
+f2 = F_[Int(round(size(F_,1)*0.45)),:]        # Yes, 10.1021/tx3001994
+f3 = F_[Int(round(size(F_,1)*0.515)),:]       # No
+f4 = F_[Int(round(size(F_,1)*0.55)),:]        # Yes, 10.1016/j.jhazmat.2020.123787
+f5 = F_[Int(round(size(F_,1)*0.964)),:]       # No
 
-a_mw = plot(Norm_raw[groupA,:].MW,seriestype=stephist,fill=true,title="Group A",bins=:auto)
-plot!([a1[2],a1[2]],[0,202],c=:red,linewidth=2)
-plot!([a2[2],a2[2]],[0,510],c=:red,linewidth=2)
-plot!([a3[2],a3[2]],[0,433],c=:red,linewidth=2)
-plot!([a4[2],a4[2]],[0,370],c=:red,linewidth=2)
-plot!([a5[2],a5[2]],[0,52],c=:red,linewidth=2)
-b_mw = plot(Norm_raw[groupB,:].MW,seriestype=stephist,fill=true,title="Group B",bins=50)
-plot!([b1[2],b1[2]],[0,213],c=:red,linewidth=2)
-plot!([b2[2],b2[2]],[0,192],c=:red,linewidth=2)
-plot!([b3[2],b3[2]],[0,180],c=:red,linewidth=2)
-plot!([b4[2],b4[2]],[0,133],c=:red,linewidth=2)
-plot!([b5[2],b5[2]],[0,47],c=:red,linewidth=2)
-c_mw = plot(Norm_raw[groupC,:].MW,seriestype=stephist,fill=true,title="Group C",bins=80,xlims=(50,600))
-plot!([c1[2],c1[2]],[0,1040],c=:red,linewidth=2)
-plot!([c2[2],c2[2]],[0,1720],c=:red,linewidth=2)
-plot!([c3[2],c3[2]],[0,1480],c=:red,linewidth=2)
-plot!([c4[2],c4[2]],[0,1480],c=:red,linewidth=2)
-plot!([c5[2],c5[2]],[0,345],c=:red,linewidth=2)
-d_mw = plot(Norm_raw[groupD,:].MW,seriestype=stephist,fill=true,title="Group D",bins=60)
-plot!([d1[2],d1[2]],[0,17],c=:red,linewidth=2)
-plot!([d2[2],d2[2]],[0,44],c=:red,linewidth=2)
-plot!([d3[2],d3[2]],[0,44],c=:red,linewidth=2)
-plot!([d4[2],d4[2]],[0,35],c=:red,linewidth=2)
-plot!([d5[2],d5[2]],[0,51],c=:red,linewidth=2)
-e_mw = plot(Norm_raw[groupE,:].MW,seriestype=stephist,fill=true,title="Group E",bins=70)
-plot!([e1[2],e1[2]],[0,3],c=:red,linewidth=2)
-plot!([e2[2],e2[2]],[0,1],c=:red,linewidth=2)
-plot!([e3[2],e3[2]],[0,3],c=:red,linewidth=2)
-plot!([e4[2],e4[2]],[0,3],c=:red,linewidth=2)
-plot!([e5[2],e5[2]],[0,2],c=:red,linewidth=2)
+a_mw = plot(Norm_raw[groupA,:].MW,seriestype=stephist,fill=true,title="Group A",c=:darkorange2,alpha=0.8,bins=:auto)
+plot!([a1[2],a1[2]],[0,203],c=:red3,linewidth=3)
+plot!([a2[2],a2[2]],[0,510],c=:green,linewidth=3)
+plot!([a3[2],a3[2]],[0,434],c=:green,linewidth=3)
+plot!([a4[2],a4[2]],[0,371],c=:green,linewidth=3)
+plot!([a5[2],a5[2]],[0,65],c=:red3,linewidth=3)
+b_mw = plot(Norm_raw[groupB,:].MW,seriestype=stephist,fill=true,c=:darkorange2,alpha=0.8,title="Group B",bins=50)
+plot!([b1[2],b1[2]],[0,213],c=:red3,linewidth=3)
+plot!([b2[2],b2[2]],[0,192],c=:red3,linewidth=3)
+plot!([b3[2],b3[2]],[0,180],c=:red3,linewidth=3)
+plot!([b4[2],b4[2]],[0,133],c=:red3,linewidth=3)
+plot!([b5[2],b5[2]],[0,57],c=:green,linewidth=3)
+c_mw = plot(Norm_raw[groupC,:].MW,seriestype=stephist,fill=true,c=:darkorange2,alpha=0.8,title="Group C",bins=80,xlims=(50,600))
+plot!([c1[2],c1[2]],[0,1040],c=:red3,linewidth=3)
+plot!([c2[2],c2[2]],[0,1719],c=:green,linewidth=3)
+plot!([c3[2],c3[2]],[0,1480],c=:green,linewidth=3)
+plot!([c4[2],c4[2]],[0,1480],c=:green,linewidth=3)
+plot!([c5[2],c5[2]],[0,345],c=:red3,linewidth=3)
+d_mw = plot(Norm_raw[groupD,:].MW,seriestype=stephist,fill=true,c=:darkorange2,alpha=0.8,title="Group D",bins=60)
+plot!([d1[2],d1[2]],[0,17],c=:green,linewidth=3)
+plot!([d2[2],d2[2]],[0,44],c=:red3,linewidth=3)
+plot!([d3[2],d3[2]],[0,44],c=:red3,linewidth=3)
+plot!([d4[2],d4[2]],[0,35],c=:green,linewidth=3)
+plot!([d5[2],d5[2]],[0,51],c=:red3,linewidth=3)
+e_mw = plot(Norm_raw[groupE,:].MW,seriestype=stephist,fill=true,c=:darkorange2,alpha=0.8,title="Group E",bins=70)
+plot!([e1[2],e1[2]],[0,3],c=:green,linewidth=3)
+plot!([e2[2],e2[2]],[0,1],c=:green,linewidth=3)
+plot!([e3[2],e3[2]],[0,3],c=:red3,linewidth=3)
+plot!([e4[2],e4[2]],[0,3],c=:green,linewidth=3)
+plot!([e5[2],e5[2]],[0,3],c=:red3,linewidth=3)
 f_=(Norm_raw[groupF,:].MW)
 f_[ismissing.(f_) .==1] .= -500
-f_mw = plot(f_,seriestype=stephist,fill=true,xlims=(0,1300),title="Group F",bins=180)
-plot!([f1[2],f1[2]],[0,2250],c=:red,linewidth=2)
-plot!([f2[2],f2[2]],[0,2490],c=:red,linewidth=2)
-plot!([f3[2],f3[2]],[0,2515],c=:red,linewidth=2)
-plot!([f4[2],f4[2]],[0,2535],c=:red,linewidth=2)
-plot!([f5[2],f5[2]],[0,330],c=:red,linewidth=2)
-a_logp = plot(Norm_raw[groupA,:].CrippenLogP,seriestype=stephist,fill=true,title="Group A",bins=:auto)
-b_logp = plot(Norm_raw[groupB,:].CrippenLogP,seriestype=stephist,fill=true,title="Group B",bins=:auto)
-c_logp = plot(Norm_raw[groupC,:].CrippenLogP,seriestype=stephist,fill=true,title="Group C",bins=:auto)
-d_logp = plot(Norm_raw[groupD,:].CrippenLogP,seriestype=stephist,fill=true,title="Group D",bins=:auto)
-e_logp = plot(Norm_raw[groupE,:].CrippenLogP,seriestype=stephist,fill=true,title="Group E",bins=70)
-f_logp = plot(Norm_raw[groupF,:].CrippenLogP,seriestype=stephist,fill=true,title="Group F",bins=:auto)
+f_mw = plot(f_,seriestype=stephist,fill=true,c=:darkorange2,alpha=0.8,xlims=(0,1300),title="Group F",bins=180)
+plot!([f1[2],f1[2]],[0,2252],c=:red3,linewidth=3)
+plot!([f2[2],f2[2]],[0,2500],c=:green,linewidth=3)
+plot!([f3[2],f3[2]],[0,2540],c=:red3,linewidth=3)
+plot!([f4[2],f4[2]],[0,2540],c=:green,linewidth=3)
+plot!([f5[2],f5[2]],[0,242],c=:red3,linewidth=3)
+a_logp = plot(Norm_raw[groupA,:].CrippenLogP,seriestype=stephist,c=:lightyellow4,alpha=0.8,fill=true,title="Group A",bins=:auto)
+b_logp = plot(Norm_raw[groupB,:].CrippenLogP,seriestype=stephist,c=:lightyellow4,alpha=0.8,fill=true,title="Group B",bins=:auto)
+c_logp = plot(Norm_raw[groupC,:].CrippenLogP,seriestype=stephist,c=:lightyellow4,alpha=0.8,fill=true,title="Group C",bins=:auto)
+d_logp = plot(Norm_raw[groupD,:].CrippenLogP,seriestype=stephist,c=:lightyellow4,alpha=0.8,fill=true,title="Group D",bins=:auto)
+e_logp = plot(Norm_raw[groupE,:].CrippenLogP,seriestype=stephist,c=:lightyellow4,alpha=0.8,fill=true,title="Group E",bins=70)
+f_logp = plot(Norm_raw[groupF,:].CrippenLogP,seriestype=stephist,c=:lightyellow4,alpha=0.8,fill=true,title="Group F",bins=:auto)
+
 l1 = @layout [z{0.1h};a b;c d; e f]
 l2 = @layout [z{0.1h} z{0.1h};a b;c d; e f]
-title1 = plot(title="Molecular weight",grid = false, showaxis = false,tick=false,bottom_margin = -50Plots.px)
-title2 = plot(title="Crippen LogP",grid = false, showaxis = false,tick=false,bottom_margin = -50Plots.px)
+l3 = @layout [a b;c d; e f]
+title1 = plot(title="Molecular weight",grid = false, showaxis = false,tick=false,bottom_margin = -50Plots.px,titlefont=16)
+title2 = plot(title="Crippen LogP",grid = false, showaxis = false,tick=false,bottom_margin = -50Plots.px,titlefont=16)
 
 plot(title1,title2,a_mw,a_logp,b_mw,b_logp,c_mw,c_logp,legend=false, c=:blue3, layout=l2,dpi=600,size=(900,600))
-plot(title1,title2,d_mw,d_logp,e_mw,e_logp,f_mw,f_logp,legend=false, c=:blue3, layout=l2,dpi=600,size=(900,600))
+sp.savefig("C:\\Users\\alex_\\Documents\\GitHub\\RTI_prediction\\Validation_MW_CLogP_ABC.png")
+
+plot(d_mw,d_logp,e_mw,e_logp,f_mw,f_logp,legend=false, c=:blue3, layout=l3,dpi=600,size=(900,600))
+sp.savefig("C:\\Users\\alex_\\Documents\\GitHub\\RTI_prediction\\Validation_MW_CLogP_DEF.png")
 
 plot(title1,a_mw,b_mw,c_mw,d_mw,e_mw,f_mw,legend=false, c=:blue3, layout=l1,dpi=600,size=(900,600))
 plot(title2,a_logp,b_logp,c_logp,d_logp,e_logp,f_logp,legend=false, c=:blue3, layout=l1,dpi=600,size=(900,600))
